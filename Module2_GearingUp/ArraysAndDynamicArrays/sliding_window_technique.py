8bb1a49bc55fa483db6d4ccc2de9b94459bd65c2
aditnag <iamaditnag034@gmail.com>
2022-11-10 21:09:56 +0530

    sliding window - minimum number of swaps to bring all the elements <= k together

--- Module2_GearingUp/ArraysAndDynamicArrays/sliding_window_technique.py
+++ Module2_GearingUp/ArraysAndDynamicArrays/sliding_window_technique.py
@@ -14,45 +14,75 @@
 # Time complexity: O(N)
 # Space complexity: O(1)
 
+import sys
+
 
 class SlidingWindowTechnique:
-    def subArraySum(self):
-        print("Enter the size of the array and the value of the window size")
+    # def subArraySum(self):
+    #     print("Enter the size of the array and the value of the window size")
+    #     n, k = map(int, input().strip().split())
+    #     ar = list(map(int, input().strip().split()))[:n]
+    #     print(f"Sum of the sub-array of size {k} is:")
+    #     sum = 0
+    #     for i in range(k):
+    #         sum += ar[i]
+    #
+    #     for i in range(k, n):
+    #         print(sum)
+    #         sum += ar[i]
+    #         sum -= ar[i - k]
+    #
+    #     return sum
+    #
+    # def frequency(self):
+    #     print("Enter the size of the array and the value of the window size")
+    #     n, k = map(int, input().strip().split())
+    #     ar = list(map(int, input().strip().split()))[:n]
+    #     print(f"Enter the element whose frequency you want to find in each array of the window size {k}")
+    #     x = int(input())
+    #
+    #     frq = 0
+    #     for i in range(k):
+    #         if ar[i] == x:
+    #             frq += 1
+    #
+    #     for i in range(k, n):
+    #         print(frq)
+    #         if ar[i] == x:
+    #             frq += 1
+    #         if ar[i - k] == x:
+    #             frq -= 1
+    #     return frq
+
+    # To find the minimum number of swaps to bring all the elements <= k together
+    def swaps(self):
+        print("Enter the size of the array and the value of K")
         n, k = map(int, input().strip().split())
+        print("Enter the array elements")
         ar = list(map(int, input().strip().split()))[:n]
-        print(f"Sum of the sub-array of size {k} is:")
-        sum = 0
-        for i in range(k):
-            sum += ar[i]
 
-        for i in range(k, n):
-            print(sum)
-            sum += ar[i]
-            sum -= ar[i - k]
+        countLegalElements = 0
+        for i in range(n):
+            if ar[i] <= k:
+                countLegalElements += 1
 
-        return sum
+        ans = sys.maxsize
+        max_count = 0
+        for i in range(countLegalElements):
+            if ar[i] <= k:
+                max_count += 1
 
-    def frequency(self):
-        print("Enter the size of the array and the value of the window size")
-        n, k = map(int, input().strip().split())
-        ar = list(map(int, input().strip().split()))[:n]
-        print(f"Enter the element whose frequency you want to find in each array of the window size {k}")
-        x = int(input())
+        for i in range(countLegalElements, n):
+            ans = min(ans, countLegalElements - max_count)
+            if ar[i] <= k:
+                max_count += 1
+            if ar[i-countLegalElements] == k:
+                max_count -= 1
 
-        frq = 0
-        for i in range(k):
-            if ar[i] == x:
-                frq += 1
-
-        for i in range(k, n):
-            print(frq)
-            if ar[i] == x:
-                frq += 1
-            if ar[i-k] == x:
-                frq -= 1
-        return frq
+        return ans
 
 
 obj = SlidingWindowTechnique()
-print(obj.subArraySum())
-print(obj.frequency())
+# print(obj.subArraySum())
+# print(obj.frequency())
+print(obj.swaps())
