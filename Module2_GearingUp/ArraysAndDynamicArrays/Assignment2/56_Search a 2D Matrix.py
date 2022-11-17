77bc998964db993f1207111090d0bec73b7c6164
aditnag <iamaditnag034@gmail.com>
2022-11-18 00:35:33 +0530

    Search a 2D Matrix

---
+++ Module2_GearingUp/ArraysAndDynamicArrays/Assignment2/56_Search a 2D Matrix.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        i = 0
+        j = len(matrix[0]) - 1
+        while i < len(matrix) and j >= 0:
+            if target == matrix[i][j]:
+                return True
+                break
+            elif target > matrix[i][j]:
+                i += 1
+            else:
+                j -= 1
