713c1d1454f3eaeb5b29e2aa120a5b1f4f170636
aditnag <iamaditnag034@gmail.com>
2022-11-23 13:20:12 +0530

    Array Rearrangement: Alternate Technique - code

--- Module2_GearingUp/ArraysAndDynamicArrays/ArrayRearrangement_alternateTechnique.py
+++ Module2_GearingUp/ArraysAndDynamicArrays/ArrayRearrangement_alternateTechnique.py
@@ -20,3 +20,31 @@
 # Can we do it the other way around ie. x = N*old_value + new_value.
 # We can not! since for any untouched element, x/N = 0 (Arr[i]<N-1), therefore we will not be able to extract the old value.
 # In the above method, we are multiplying N with the new_value, which may lead to integer overflow if N is larger than 10^4. Therefore, it may not work in every case.
+
+class ArrayRearrangementAT:
+    def main(self):
+        print("Enter the length of the array")
+        n = int(input())
+
+        print("Enter the array elements")
+        ar = list(map(int, input().strip().split()))[:n]
+
+        """"
+        To do: if ar[i] = j then ar[j] = i. 
+        For this the elements should be permutation of n (size of the array) and less than n
+        """
+        for i in range(n):
+            old_value = ar[i] % n
+            ar[old_value] = n * i + ar[old_value]
+
+        # new arrangement
+        for i in range(n):
+            ar[i] = ar[i] // n
+
+        print("New arrangement:")
+        for elements in ar:
+            print(elements, end=" ")
+
+
+obj = ArrayRearrangementAT()
+obj.main()
