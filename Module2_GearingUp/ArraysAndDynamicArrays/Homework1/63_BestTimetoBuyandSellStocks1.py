05e4c825da7b7befe31194cf40a4dd43060ff569
aditnag <iamaditnag034@gmail.com>
2022-11-25 00:25:22 +0530

    HW: Best Time to Buy and Sell Stocks 1 - corrct code

--- Module2_GearingUp/ArraysAndDynamicArrays/Homework1/63_BestTimetoBuyandSellStocks1.py
+++ Module2_GearingUp/ArraysAndDynamicArrays/Homework1/63_BestTimetoBuyandSellStocks1.py
@@ -3,20 +3,15 @@ from math import inf
 
 
 class OptimizedTime:
-    def suffixmax(self, arr: list, a: int):
-        for i in range(len(arr) - 2, a - 1, -1):
-            arr[i] = max(arr[i + 1], arr[i])
-
-        for i in range(a, len(arr)):
-            return arr[i]
-
     def main(self):
         n = int(input())
         ar = list(map(int, input().strip().split()))[:n]
         smax = ar.copy()
+        for i in range(n - 2, - 1, -1):
+            smax[i] = max(smax[i + 1], smax[i])
         ans = -inf
         for i in range(n - 1):
-            ans = max(ans, self.suffixmax(smax, i + 1) - ar[i])
+            ans = max(ans, smax[i + 1] - ar[i])
 
         if ans <= 0:
             return 0
