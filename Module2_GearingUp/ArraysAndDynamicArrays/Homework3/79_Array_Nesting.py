f649f6274ba9badcf4464ba66741938e66271d87
adit <adit.n@pacewisdom.com>
2023-06-09 18:17:16 +0530

    ArrayNesting

--- Module2_GearingUp/ArraysAndDynamicArrays/Homework3/79_Array_Nesting.py
+++ Module2_GearingUp/ArraysAndDynamicArrays/Homework3/79_Array_Nesting.py
@@ -1,20 +1,21 @@
 # cook your dish here
 
 class ArrayNesting:
-    def findCount(self, nums, n):
-        count = 0
-        ar = []
-        index = 0
-        for i in range(n):
-            if nums[index] not in ar:
-                ar.append(nums[index])
-                index = nums[index]
+    def findCount(self, nums):
+        ans = 0
+        for num in nums:
+            count = 0
+            while nums[num] != -1:
                 count += 1
-        print(count)
+                temp = num
+                num = nums[num]
+                nums[temp] = -1
+            ans = max(ans, count)
+        print(ans)
 
 
 if __name__ == '__main__':
     obj = ArrayNesting()
     n = int(input())
     nums = list(map(int, input().strip().split()))[:n]
-    obj.findCount(nums, n)
+    obj.findCount(nums)
