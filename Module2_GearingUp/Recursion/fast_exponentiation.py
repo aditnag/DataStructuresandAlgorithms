23928a83f2e22399ac6ff7c14957322091d2028a
adit <adit.n@pacewisdom.com>
2023-07-07 12:04:33 +0530

    recursion: Fast Exponentiation

---
+++ Module2_GearingUp/Recursion/fast_exponentiation.py
@@ -0,0 +1,17 @@
+class FastExponentiation:
+    def fastexp(self, n: int, k: int):
+        if k == 0:
+            return 1
+        x = self.fastexp(n, k // 2)
+        if k % 2 == 0:
+            return x * x
+        return x * x * n
+
+    def main(self):
+        n, k = map(int, input().strip().split())
+        result = self.fastexp(n, k)
+        print(result)
+
+
+obj = FastExponentiation()
+obj.main()
