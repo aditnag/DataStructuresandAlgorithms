870fa93d74b6ca0e49fc9f560e93d9849ab38a6f
aditnag <iamaditnag034@gmail.com>
2023-09-25 13:18:43 +0530

    2P

---
+++ Module3/Two Pointers/Assignment_1/Pair with a Difference.py
@@ -0,0 +1,22 @@
+# cook your dish here
+def pairDiff(nums, k):
+    nums.sort()
+    n = len(nums)
+    i = 0
+    j = 1
+    while j < n:
+        diff = nums[j] - nums[i]
+        if diff == k:
+            return True
+        elif diff < k:
+            j += 1
+        else:
+            i += 1
+
+
+n, k = map(int, input().split())
+arr = list(map(int, input().strip().split()))[:n]
+if pairDiff(arr, k):
+    print("Yes")
+else:
+    print("No")
